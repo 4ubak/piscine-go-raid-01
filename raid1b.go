9f577bd567a4bae7b93cda9efe6e3dcace0f5f1d
Temirlan <ib.temirlan@gmail.com>
2019-10-19 17:36:37 +0600

    2 - task

---
+++ raid1b.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	Raid1a(1, 1)
+}
+
+func Raid1a(x, y int) {
+	for a := 1; a <= y; a++ {
+		for b := 1 ; b <= x ; b++ {
+			if (a == 1 && b == 1) {
+				fmt.Printf("%s", "/")
+			} else if (a == 1 && b == x) {
+				z01.PrintRune(92)
+			} else if (a == y && b == 1) {
+				z01.PrintRune(92)
+			} else if (a ==y && b == x) {
+				fmt.Printf("%s", "/")
+			} else if b == 1 && (a != y || a != 1) {
+				fmt.Printf("%s", "*")
+			} else if b == x && (a != 1 || a != y) {
+				fmt.Printf("%s", "*")
+			} else if a == 1 && (b != 1 || b != x) {
+				fmt.Printf("%s", "*")
+			} else if a == y && (b != 1 || b != x) {
+				fmt.Printf("%s", "*")
+			} else {
+				fmt.Printf("%s", " ")
+			}
+			
+		}
+		fmt.Println()
+	}
+}
