75c92d87388d39989d41ce70387ce99e60f8a111
Temirlan <ib.temirlan@gmail.com>
2019-10-19 17:38:13 +0600

    2 - task

--- raid1b.go
+++ raid1b.go
@@ -1,38 +1,36 @@
-package main
+package piscine
 
-import (
-	"fmt"
-	"github.com/01-edu/z01"
-)
-
-func main() {
-	Raid1a(1, 1)
-}
+import "github.com/01-edu/z01"
 
 func Raid1a(x, y int) {
-	for a := 1; a <= y; a++ {
-		for b := 1 ; b <= x ; b++ {
-			if (a == 1 && b == 1) {
-				fmt.Printf("%s", "/")
-			} else if (a == 1 && b == x) {
-				z01.PrintRune(92)
-			} else if (a == y && b == 1) {
-				z01.PrintRune(92)
-			} else if (a ==y && b == x) {
-				fmt.Printf("%s", "/")
-			} else if b == 1 && (a != y || a != 1) {
-				fmt.Printf("%s", "*")
-			} else if b == x && (a != 1 || a != y) {
-				fmt.Printf("%s", "*")
-			} else if a == 1 && (b != 1 || b != x) {
-				fmt.Printf("%s", "*")
-			} else if a == y && (b != 1 || b != x) {
-				fmt.Printf("%s", "*")
-			} else {
-				fmt.Printf("%s", " ")
+	if x <= 0 || y <= 0 {
+
+		} else {
+			for a := 1; a <= y; a++ {
+				for b := 1; b <= x; b++ {
+					if a == 1 && b == 1 {
+						fmt.Printf("%s", "/")
+					} else if a == 1 && b == x {
+						z01.PrintRune(92)
+					} else if a == y && b == 1 {
+						z01.PrintRune(92)
+					} else if a == y && b == x {
+						fmt.Printf("%s", "/")
+					} else if b == 1 && (a != y || a != 1) {
+						fmt.Printf("%s", "*")
+					} else if b == x && (a != 1 || a != y) {
+						fmt.Printf("%s", "*")
+					} else if a == 1 && (b != 1 || b != x) {
+						fmt.Printf("%s", "*")
+					} else if a == y && (b != 1 || b != x) {
+						fmt.Printf("%s", "*")
+					} else {
+						fmt.Printf("%s", " ")
+					}
+	
+				}
+				fmt.Println()
 			}
-			
 		}
-		fmt.Println()
 	}
 }
