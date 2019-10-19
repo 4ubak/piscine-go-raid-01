9324be7de4c3c1c3664745765d694dd4df764cb2
Temirlan <ib.temirlan@gmail.com>
2019-10-19 17:14:52 +0600

    1 - task

--- raid1a.go
+++ raid1a.go
@@ -3,17 +3,17 @@ package piscine
 import "github.com/01-edu/z01"
 
 func Raid1a(x, y int) {
-	for a := 1; a <= x; a++ {
-		for b := 1; b <= y; b++ {
-			if (a == 1 && b == 1) || (a == 1 && b == y) || (a == x && b == 1) || (a == x && b == y) {
+	for a := 1; a <= y; a++ {
+		for b := 1; b <= x; b++ {
+			if (a == 1 && b == 1) || (a == 1 && b == x) || (a == y && b == 1) || (a == y && b == x) {
 				z01.PrintRune('o')
-			} else if b == 1 && (a != x || a != 1) {
+			} else if b == 1 && (a != y || a != 1) {
 				z01.PrintRune('|')
-			} else if b == y && (a != 1 || a != x) {
+			} else if b == x && (a != 1 || a != y) {
 				z01.PrintRune('|')
-			} else if a == 1 && (b != 1 || b != y) {
+			} else if a == 1 && (b != 1 || b != x) {
 				z01.PrintRune('-')
-			} else if a == x && (b != 1 || b != y) {
+			} else if a == y && (b != 1 || b != x) {
 				z01.PrintRune('-')
 			} else {
 				z01.PrintRune(' ')
