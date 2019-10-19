e760e02bf396da37672264d51af06ee2998f0f85
Temirlan <ib.temirlan@gmail.com>
2019-10-19 17:50:29 +0600

    3 - task

--- raid1c.go
+++ raid1c.go
@@ -7,10 +7,10 @@ func Raid1c(x, y int) {
 
 	} else {
 		for a := 1; a <= y; a++ {
-			for b := 1 ; b <= x ; b++ {
+			for b := 1; b <= x; b++ {
 				if (a == 1 && b == 1) || (a == 1 && b == x) {
 					z01.PrintRune('A')
-				} else if (a == y && b == x) || (a == y && b ==1) {
+				} else if (a == y && b == x) || (a == y && b == 1) {
 					z01.PrintRune('C')
 				} else if b == 1 && (a != y || a != 1) {
 					z01.PrintRune('B')
