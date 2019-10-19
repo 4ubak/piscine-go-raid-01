36604f3166c688c7874c4f6d714be1b6a957dacf
Temirlan <ib.temirlan@gmail.com>
2019-10-19 17:45:25 +0600

    2 - task

--- raid1b.go
+++ raid1b.go
@@ -9,23 +9,23 @@ func Raid1a(x, y int) {
 		for a := 1; a <= y; a++ {
 			for b := 1; b <= x; b++ {
 				if a == 1 && b == 1 {
-					fmt.Printf("%s", "/")
+					z01.PrintRune('/')
 				} else if a == 1 && b == x {
 					z01.PrintRune(92)
 				} else if a == y && b == 1 {
 					z01.PrintRune(92)
 				} else if a == y && b == x {
-					fmt.Printf("%s", "/")
+					z01.PrintRune('/')
 				} else if b == 1 && (a != y || a != 1) {
-					fmt.Printf("%s", "*")
+					z01.PrintRune('*')
 				} else if b == x && (a != 1 || a != y) {
-					fmt.Printf("%s", "*")
+					z01.PrintRune('*')
 				} else if a == 1 && (b != 1 || b != x) {
-					fmt.Printf("%s", "*")
+					z01.PrintRune('*')
 				} else if a == y && (b != 1 || b != x) {
-					fmt.Printf("%s", "*")
+					z01.PrintRune('*')
 				} else {
-					fmt.Printf("%s", " ")
+					z01.PrintRune(10)
 				}
 			}
 			fmt.Println()
